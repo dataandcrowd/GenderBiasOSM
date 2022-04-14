555d0595f7c3dc46e718e2d9a64c8d5983cc6992
Hyesop <shinhyesop@gmail.com>
2022-04-14 17:22:29 +0100

    update R code for leaflet

--- test.R
+++ test.R
@@ -21,7 +21,9 @@ simpson <- load("Simpson_GISRUK.RData")
 
 bind_rows(country_list) %>% 
   group_by(Country_full) %>% 
-  summarise(Changeset = sum(Changeset)) -> country_df
+  summarise(Changeset = sum(Changeset)) %>% 
+  mutate(Country_full = case_when(Country_full == "United States" ~ "United States of America",
+                                  TRUE ~ as.character(Country_full))) -> country_df
 
 
 world <- ne_countries(scale = "medium", returnclass = "sf") %>% 
@@ -31,12 +33,12 @@ world_sp <- world %>% as_Spatial()
 
 m <- 
   leaflet(world_sp) %>% 
-  setView(lng = 0, lat = 52, zoom = 1) %>% 
+  setView(lng = 0, lat = 52, zoom = 5) %>% 
   addTiles()
 
 
 
-bins <- c(0, 500, 1000, 2000, 5000, 10000, 50000, Inf)
+bins <- c(0, 1000, 5000, 10000, 50000, Inf)
 pal <- colorBin("YlOrRd", domain = world_sp$Changeset, bins = bins)
 
 m %>% 
